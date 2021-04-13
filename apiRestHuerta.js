8f2de411557f973d91b089e09440aa50c54b9461
pascual <pjbolullo@gmail.com>
2021-04-13 10:21:06 +0200

    Endpoints del user db

--- apiRestHuerta.js
+++ apiRestHuerta.js
@@ -31,7 +31,7 @@ connection.connect(function (err){
         console.log("Conexión correcta.")
     }
 });
-/*////////////////////////////////////*/ 
+/*///////////////GET USER/////////////////////*/ 
 
 app.get("/user",
 
@@ -67,5 +67,95 @@ function (request,response)
         }) 
     }
 });
+/*/////////////////POST USER///////////////////*/
+app.post ("/user",
+
+function (request,response) 
+{
+    let params = [request.body.name, request.body.surname1, request.body.surname2, request.body.birthyear, request.body.username, request.body.localidad, request.body.cp, request.body.email, request.body.password]
+    let usuario = "INSERT INTO user (name, surname1, surname2, birthyear, username, localidad, cp, email, password) VALUES (?,?,?,?,?,?,?,?,?)";
+    connection.query(usuario, params, function (err, result)
+    {
+        if (err)
+        {
+            if((err.code == "ER_BAD_NULL_ERROR"))
+            {
+                response.send({"mensaje":"No se puede introducir un valor nulo"})
+            }
+            else if (err.code == "ER_NO_REFERNCED_ROW_2")
+            {
+                response.send({"mensaje":"Introduce un grupo válido"})
+            }
+            else {response.send(err)}
+        }
+        else 
+        {
+            let respuesta = {"mensaje":"Usuario añadido"}
+            response.send(respuesta)
+        }
+    })
+});
+
+/*/////////////////PUT USER///////////////////*/
+
+app.put("/user",
+
+function (request,response) 
+{
+    let params = [request.body.name, request.body.surname1, request.body.surname2, request.body.birthyear, request.body.username, request.body.localidad, request.body.cp, request.body.email, request.body.password, request.body.iduser]
+    if (request.body.iduser == null)
+    {
+        response.send({"mensaje":"Introduce un ID"})
+    }
+    else
+    {
+        let usuario = "UPDATE user SET name = COALESCE(?, name), surname1 = COALESCE (?, surname1), surname2 = COALESCE(?, surname2), birthyear = COALESCE(?, birthyear), username = COALESCE(?, username), localidad = COALESCE(?, localidad), cp = COALESCE(?, cp), email = COALESCE(?, email), password = COALESCE(?, password) WHERE iduser = ?";
+    
+    connection.query(usuario, params, function (err, result)
+    {
+        if (err) 
+        {
+            if (err)
+            {
+                response.send(err)
+            }
+        }
+        else {response.send({"mensaje":"Modificación aplicada"})}
+    });
+    }
+});
+
+/*/////////////////DELETE USER///////////////////*/
+
+app.delete("/user",
+
+function (request,response) 
+{
+    let params = [request.query.id]
+    let user = "DELETE FROM user WHERE iduser = ?";
+    connection.query(user, params, function (err, result)
+    {
+        if (err) 
+        {
+            response.send(err)
+        }
+        
+        else
+        {
+            if(response.affectedRows == 0)
+            {
+                response.send ({"mensaje":"El ID introducido no es válido"})
+            }
+            else 
+            {
+                response.send ({"mensaje":"Usuario eliminado"})}
+            }
+        })
+});
+
+app.use(function(request, response, next){
+    respuesta = {codigo: 404, mensaje: "URL no encontrada"}
+    response.status(404).send(respuesta)
+})
 
 app.listen(port);
